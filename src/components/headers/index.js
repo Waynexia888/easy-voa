a0849dd6dcddb55b3ae417b2dd02a942df1964ed
Waynexia888 <qiwuwaynexia@gmail.com>
2020-03-31 17:28:15 -0700

    first commit

---
+++ src/components/headers/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './easyvoa.jpeg'
+
+class AppHeader extends Component {
+    render() {
+        return (
+           <img src={logo} />
+        )
+    }
+}
+
+export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+
+import './style.css';
+import AppHeader from './components/headers/index'
+
+const { Header, Footer, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Layout style={{ minWidth: 1300 }}>
+        <Header className="header">
+          <AppHeader />
+        </Header>
+        <Content className="content">Content</Content>
+        <Footer className="footer">Footer</Footer>
+      </Layout>
+    );
+  }
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
